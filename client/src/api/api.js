552ea8c9410293936da1a9616816e9b6bff5e1cd
Naavya <naavyachaturvedi09@gmail.com>
2026-08-08 21:48:41 +0530

    Connect frontend to deployed API

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://campusdesk-api-ih0s.onrender.com/api",
   timeout: 10000,
 });
 
